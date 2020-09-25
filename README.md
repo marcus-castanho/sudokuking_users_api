77955e26b564ddce7e158e97f24186203204408a
Marcus Castanho <castanhomarcus@gmail.com>
2020-09-24 23:50:28 -0300

    Readme update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Sudoku King users API
-An API to register, authenticate and manage users of the Sudoku King app
+An API to register, authenticate and manage users of the [Sudoku King app](https://github.com/marcus-castanho/sudokuking). This API in being developed with NodeJS and a NoSQL database MongoDB.
+
+## Features in developement
+- CRUD routines for users of the Sudoku King app
+- E-mail validation for new users
+- Score record of users game sessions
+
+## Stack
+- Language/runtime environement: JS/[NodeJS] (https://nodejs.org/en/)
+- Database: [MongoDB] (https://www.mongodb.com/)
+- Library to handle database objects: [Mongoose] (https://github.com/Automattic/mongoose)
+- Framework to handle requests: [Express] (https://github.com/expressjs/express)
--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const User = require('../models/User')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = ('../config/auth.json');
 
 const router = express.Router();
 
@@ -8,17 +11,44 @@ router.post('/register', async (req, res) => {
         const { email } = req.body;
 
         if (await User.findOne({ email }))
-            return res.status(400).send({ error: 'E-mail already registered' });
+            return res.status(400).json({ error: 'E-mail already registered' });
 
         const user = await User.create(req.body);
 
         user.password = undefined;
 
-        return res.send({ user });
+        return res.json({ user });
     }
     catch (err) {
-        return res.status(400).send({ error: 'Registration failed' })
+        return res.status(400).json({ error: 'Registration failed' })
     }
 });
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select('+password');
+    try {
+        if (!user)
+            return res.status(400).json({ error: 'User not found' })
+        if (!await bcrypt.compare(password, user.password))
+            return res.status(400).json({ error: 'Invalid password' })
+
+        /*const token = jwt.sign({ id: user._id }, authConfig.secret, {
+            expiresIn: 86400,
+        });*/
+
+        const token = Object.keys(authConfig).length;
+        console.log('1');
+        console.log(authConfig);
+        //Cannot read "secret" property from JSON "auth.json", it keeps requiring and gettind undefined
+
+        user.password = undefined;
+        return res.json({ user, token });
+
+    }
+    catch (err) {
+        return res.status(400).json({ error: 'Authentication failed' })
+    }
+})
 
 module.exports = app => app.use('/auth', router);
